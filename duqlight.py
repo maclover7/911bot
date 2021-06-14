39c4e4123dbdf58c7c801bb461a5e692f604b092
Jon Moss <me@jonathanmoss.me>
2021-06-13 22:59:12 -0400

    Add duqlight checker

---
+++ duqlight.py
@@ -0,0 +1,26 @@
+import json
+from urllib import request
+import os
+from datetime import datetime
+import re
+
+req = request.Request("https://www.duquesnelight.com/outages-safety/current-outages/GetReportedOutages/")
+response = request.urlopen(req)
+data = json.loads(response.read())
+
+for outage_group in data.get("OutageGroups", []):
+    if outage_group["Municipality"]["Zip"] == "15213":
+        affected = outage_group["Affected"]
+        m = re.search("(\d+)", outage_group["LastUpdated"])
+        time = datetime.utcfromtimestamp(int(m.group(0)) / 1000).strftime("%I:%M %p")
+
+        data = {
+            'text': "As of %s, there are %i Duquesne Light customers without power in 15213." % (time, affected),
+            'username': "lightbot"
+        }
+        data = json.dumps(data)
+        data = str(data)
+        data = data.encode('utf-8')
+
+        req = request.Request(os.environ['SLACK_WEBHOOK_URL'], data=data)
+        resp = request.urlopen(req)
